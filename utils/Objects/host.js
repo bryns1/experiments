98444ca7e79dd38a0db875fbab7a7e70ff433db8
Bryn <bryn@ed.com.au>
2018-07-07 14:26:08 +1000

    Update host

--- utils/Objects/host.js
+++ utils/Objects/host.js
@@ -1,12 +1,11 @@
 // Object util
-
 function host(obj){
   const self = {}
 
   // Safely access properties
-  self.get = p => {
+  self.get = (p = '') => {
     let pointer = obj
-    let paths = p.split('.')
+    let paths = p.split('.').filter(Boolean)
     for (const path of paths){
       if (typeof pointer !== 'object') return pointer
       pointer = pointer[path]
@@ -23,6 +22,27 @@ function host(obj){
       return !Boolean(Object.keys(obj).length)
     }
     return !Boolean(obj)
+  }
+
+  self.set = (p, val) => {
+    let pointer = obj
+    let paths = p.split('.')
+    let lastPath
+
+    paths.forEach((path, i) => {
+      lastPath = path
+      if(typeof pointer[path] === 'undefined'){
+        pointer[path] = {}
+      }
+
+      if(paths[i + 1]){
+        pointer = pointer[path]
+      }
+    })
+
+    pointer[lastPath] = val
+
+    return self
   }
 
   return self
