649512632b8074df2c8bd872bde075bb5f76e07b
Bryn <brynshanahan17@gmail.com>
2018-07-12 16:02:20 +1000

    Host

--- utils/Objects/host.js
+++ utils/Objects/host.js
@@ -1,10 +1,32 @@
 // Object util
+const clone = (...args) => Object.assign({}, ...args)
+
+function addMethods(self, ...methods){
+  const all = {}
+  methods.forEach(method => {
+    Object.assign(all, {
+      [method](p){
+        return Object[method](self.get(p))
+      }
+    })
+  })
+
+  all.keys().forEach(key => {
+    all[key] = all[key].bind(self)
+  })
+
+  Object.assign(self, all)
+}
+
+// Object util
 class Host{
   constructor(obj){
     this.obj = obj
+
+    addMethods(this, 'keys', 'entries')
   }
   get (p = '') {
-    let pointer = this.obj
+    let pointer = clone(this.obj)
     let paths = p.split('.').filter(Boolean)
     for (const path of paths){
       if (typeof pointer !== 'object') return pointer
@@ -22,7 +44,8 @@ class Host{
     return !Boolean(this.obj)
   }
   set (p, val) {
-    let pointer = this.obj
+    const obj = clone(this.obj)
+    let pointer = obj
     let paths = p.split('.')
     let lastPath
 
@@ -39,7 +62,32 @@ class Host{
 
     pointer[lastPath] = val
 
+    this.obj = obj
+
     return this
+  }
+  entries(p){
+    return Object.entries(this.get(p))
+  }
+  keys(p){
+    return Object.keys(this.get(p))
+  }
+  values(p){
+    return Object.values(this.get(p))
+  }
+  map(...args){
+    let h = clone(this.obj)
+    let fn = args[0]
+
+    console.log(h)
+
+    if(args.length > 1){
+      const p = args[0]
+      fn = args[1]
+
+      h = Object.entries(this.get(p))
+    }
+    return h.reduce((acc, ...rest) => acc.push(fn(...rest)), [])
   }
 }
 
